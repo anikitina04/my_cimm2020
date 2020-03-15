365d35989af9bb4e42206c89d214a7cea23dd7ff
AnnaN <45592172+anikitina04@users.noreply.github.com>
2020-03-15 21:52:29 +0200

    Exercise 4.10 solution
    - osc_energy calculates with vectors
    - system solution using FE and EC methods

---
+++ osc_EC_energy.m
@@ -0,0 +1,29 @@
+%% Exercise 4.10
+% A script for caclulating sum of potential and kinetic energies of the
+% system using Forward Euler method.
+
+omega = 2;
+period = 2 * pi / omega;
+dt = period / 20;
+T = 30 * period;
+t = 0:dt:T;
+
+% Preallocation
+u = zeros(1, length(t));
+v = zeros(1, length(t));
+
+% Initial conditions
+X_0 = 2;
+u(1) = X_0;
+v(1) = 0;
+
+for ii = 1:length(t)-1
+   v(ii+1) = v(ii) - dt.*omega.^2.*u(ii);
+   u(ii+1) = u(ii) + dt.*v(ii+1);
+end
+
+[U, K] = osc_energy(u, v, omega);
+plot(t, U+K)
+xlabel("Time")
+ylabel("Sum of potential and kinetic energies")
+title("Sum of potential and kinetic energis using EC method")
--- osc_energy.m
+++ osc_energy.m
@@ -1,6 +1,6 @@
 function [pot_en, kin_en] = osc_energy(u, v, omega)
 
-    pot_en = 0.5 * omega^2 * u^2;
-    kin_en = 0.5 * v^2;
+    pot_en = 0.5 .* omega^2 .* u.^2;
+    kin_en = 0.5 .* v.^2;
 
 end
